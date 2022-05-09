c24e8c03365a7c7380db2c22ef3a8dae1f10a0ae
Bada tomiwa <tomiwabd@gmail.com>
2022-05-09 11:07:39 -0400

    Mobile Alignment and some functions

--- mediclab.js
+++ mediclab.js
@@ -1,4 +1,4 @@
-var accordion = document.querySelector(".accordion");
+var accordion = document.getElementsByClassName("accordion");
 var panel = document.querySelector(".panel");
 function dropdown() {
     if (panel.style.display === "none") {
@@ -8,4 +8,10 @@ function dropdown() {
         panel.style.display = "none"
     }
 };
-document.querySelector(".accordion").addEventListener("onclick", dropdown());
+function testing(){
+    console.log("Tomiwa")
+}
+accordion[0].addEventListener("click", dropdown);
+accordion[1].addEventListener("click", dropdown);
+accordion[2].addEventListener("click", dropdown);
+accordion[3].addEventListener("click", dropdown);
