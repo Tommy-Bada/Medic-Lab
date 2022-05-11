a4c6ca56d2d41f6210ff20cbc34870c9e7ac09fb
Bada tomiwa <tomiwabd@gmail.com>
2022-05-11 06:55:11 -0400

    Hambuger Menu and Footer alignment for mobile

--- mediclab.js
+++ mediclab.js
@@ -1,3 +1,4 @@
+//Accordion
 var accordion = document.getElementsByClassName("accordion");
 var panel = document.querySelector(".panel");
 function dropdown() {
@@ -15,3 +16,16 @@ accordion[0].addEventListener("click", dropdown);
 accordion[1].addEventListener("click", dropdown);
 accordion[2].addEventListener("click", dropdown);
 accordion[3].addEventListener("click", dropdown);
+
+//Hamburger Menu
+function hamburgerMenu(){
+    let navBar = document.getElementById("navbar");
+    if (navBar.style.display === "none"){
+        navBar.style.display = "block";
+    }
+    else{
+        navBar.style.display = "none";
+    }
+}
+let menuButton = document.getElementById("hamburger-menu");
+menuButton.addEventListener("click", hamburgerMenu);
